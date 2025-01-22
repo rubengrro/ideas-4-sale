3b60f06367bd4a8e72a0ff82b02225bb51d3414e
Rubén Guerrero <rub.guerrero.esp@outlook.com>
2025-01-21 22:39:25 -0600

    WIP hero section

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Start To Monetize Your<br />Business Ideas</h1>
+        <p className="sub-heading !max-w-3xl">Upload and describe your business idea and found potential customers</p>
+      </section>
+    </>
+    );
+}
